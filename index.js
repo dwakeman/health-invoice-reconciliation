ada5f358aa925b2277da6b85d17acd67cc752d11
Dave Wakeman <dwakeman@us.ibm.com>
2019-05-08 16:17:40 -0500

    Initial version

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const InvoiceReconContract = require('./lib/invoice-recon');
+
+module.exports.InvoiceReconContract = InvoiceReconContract;
+module.exports.contracts = [ InvoiceReconContract ];
